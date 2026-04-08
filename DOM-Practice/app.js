bf8a9b0ac755419e480190c0a846fbd29c2a5d3a
sigma-student-10 <mdafjolhossain20@gmail.com>
2026-04-08 16:33:20 +0600

    first commit

---
+++ DOM-Practice/app.js
@@ -0,0 +1,20 @@
+// Selecting Element by Image changed
+
+// let smallImages = document.getElementsByClassName("oldImg");
+
+// for (let i=0; i < smallImages.length; i++) {
+//     smallImages[i].src="Images/spiderman_img (2).png";
+//     console.log(`value of image no. ${i} is changed.`);
+// }
+
+// Query Selectors
+
+// console.dir(document.querySelector(".oldImg"));
+
+// console.dir(document.querySelectorAll("p"));
+
+// console.dir(document.querySelector("#description"));
+
+// console.dir(document.querySelectorAll("div a"));
+
+
