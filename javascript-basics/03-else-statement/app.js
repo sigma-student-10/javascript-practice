38d8c7a71188f5d8d7d924073f1e487a55d71315
sigma-student-10 <mdafjolhossain20@gmail.com>
2026-04-15 19:53:00 +0600

    add new code

---
+++ javascript-basics/03-else-statement/app.js
@@ -0,0 +1,14 @@
+let size = "XL";
+
+if(size === "XL") {
+    console.log("price is TK.250");
+}
+ else if (size === "L"){
+    console.log("price is TK. 200");
+}
+else if(size === "M"){
+    console.log("Price is TK. 100");
+}
+else {
+    console.log("price is TK. 50");
+}
