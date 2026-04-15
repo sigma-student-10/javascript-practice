c85190438796201ba6fa695b14963a800dce893a
sigma-student-10 <mdafjolhossain20@gmail.com>
2026-04-15 20:05:02 +0600

    add new file

---
+++ javascript-basics/04-nested-if-else/app.js
@@ -0,0 +1,12 @@
+let marks = 70;
+
+if(marks >= 33) {
+    console.log("pass");
+    if(marks >= 80) {
+        console.log("Grade: A+");
+    } else {
+        console.log("Grade: A");
+    }
+} else {
+    console.log("Better luck next time");
+}
