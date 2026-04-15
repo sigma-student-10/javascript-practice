3c2bca704d33ef7ad55c41bea8417cbcef9307e3
sigma-student-10 <mdafjolhossain20@gmail.com>
2026-04-15 19:38:56 +0600

    add new code

---
+++ javascript-basics/if-statement/app.js
@@ -0,0 +1,12 @@
+let color = "green";
+
+if(color ==="red") {
+    console.log("stop");
+}
+
+if(color === "yellow"){
+    console.log("show down");
+}
+if(color === "green"){
+    console.log("go");
+}
