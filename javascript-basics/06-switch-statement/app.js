3ebb3eb1c5de4f68d5fb6cd281b03a992c0270d9
sigma-student-10 <mdafjolhossain20@gmail.com>
2026-04-17 09:01:35 +0600

    add new file

---
+++ javascript-basics/06-switch-statement/app.js
@@ -0,0 +1,17 @@
+let day = 1;
+switch(day) {
+    case 1:
+        console.log("monday");
+        break;
+        
+        case 2:
+            console.log("tuesday");
+            break;
+
+        case 3:
+            console.log("thursday");
+            break;
+
+        default:
+            console.log("wrong day")
+}
